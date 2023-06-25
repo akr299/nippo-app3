ab73f03b1ec32a5355df65986a398c7aceb2388b
akr299 <akira.ohira@el-ltd.co.jp>
2023-06-25 17:38:24 +0900

    init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,218 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React, { useCallback, useEffect, useState } from "react";
+import { useGmail } from "../hooks/useGmail";
+import { useRouter } from "next/router";
+import axios from "axios";
 
-const inter = Inter({ subsets: ['latin'] })
+const yourmailaddress = "bookreader0299@gmail.com";
 
-export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+interface UserProfileRes {
+  emailAddress: string;
+  messagesTotal: number;
+  threadsTotal: number;
+  historyId: string;
 }
+
+interface MailListRes {
+  messages: MessageList[];
+  nextPageToken: string;
+  resultSizeEstimate: number;
+}
+
+interface MessageList {
+  id: string;
+  threadId: string;
+}
+
+interface Message {
+  id: string;
+  threadId: string;
+  labelIds: [string];
+  snippet: string;
+  historyId: string;
+  internalDate: string;
+  sizeEstimate: number;
+  raw: string;
+  payload: {
+    partId: string;
+    mimeType: string;
+    filename: string;
+    headers: [
+      {
+        name: string;
+        value: string;
+      }
+    ];
+    body: {
+      attachmentId: string;
+      size: number;
+      data: string;
+    };
+  };
+}
+
+const Index = () => {
+  // ユーザーのプロフィール
+  const [profile, setProfile] = useState<UserProfileRes | undefined>(undefined);
+
+  // mailListで取得した結果。idとthreadidしかない。
+  const [mailList, setMailList] = useState<MailListRes | undefined>(undefined);
+
+  // mailListで取得した結果から、詳細を取得して格納
+  const [mails, setMails] = useState<Message[]>([]);
+
+  const { gmailAuthEndpoint, toAuthCode, res, hasError, invalidCode } =
+    useGmail();
+
+  // リダイレクトされたときにurlにcodeがのっかってくるのでuseRouterでクエリを取得する。
+  // http://localhost:3000/login?code=111jasha　みたいな感じで返ってくる。
+  const router = useRouter();
+  const { code } = router.query;
+
+  // クエリでcodeが取れた場合、AccessTokenと交換する。
+  useEffect(() => {
+    if (code != null) {
+      toAuthCode(code as string);
+    }
+  }, [code]);
+
+  // プロフィールを取得するapiのendpoint。yourmailaddressにはOAuthで入力したメールアドレスを入力。
+  const endpoint = `https://gmail.googleapis.com/gmail/v1/users/${yourmailaddress}/profile`;
+
+  // メールのリストを取得するapiのendpoint。yourmailaddressにはOAuthで入力したメールアドレスを入力。
+  // 以下apiのResponceはidとthreadidしかないため、データが取れた後詳細をgetする
+  const mailEndpoint = `https://gmail.googleapis.com/gmail/v1/users/${yourmailaddress}/messages`;
+
+  // profileを取得する関数
+  const getProfile = useCallback(() => {
+    if (res?.access_token != null) {
+      axios
+        .get<UserProfileRes>(endpoint, {
+          headers: {
+            Authorization: `Bearer ${res.access_token}`,
+          },
+        })
+        .then((res) => setProfile(res.data));
+    }
+  }, [res]);
+
+  // メールのリストを取得する関数
+  const getEmail = useCallback(() => {
+    if (res?.access_token != null) {
+      axios
+        .get<MailListRes>(mailEndpoint, {
+          headers: {
+            Authorization: `Bearer ${res.access_token}`,
+          },
+          params: {
+            maxResults: 500,
+            includeSpamTrash: false,
+            q: "After: 2022/03/09 ",
+          },
+        })
+        .then((res) => setMailList(res.data));
+    }
+  }, [res]);
+
+  // mailListが変わったタイミング→メールのリストを取得したタイミングで発火
+  useEffect(() => {
+    if (mailList != null && res != null) {
+      // Promise<AxiosResponse<Message>>の配列が返ってくるため後ほどPrimise.allをする必要あり。
+      const data = mailList.messages.map(async (mail: MessageList) => {
+        // yourmailaddressはOAuthで使用したアドレス。
+        const endpoint = `https://gmail.googleapis.com/gmail/v1/users/${yourmailaddress}/messages/${mail.id}`;
+        return await axios.get<Message>(endpoint, {
+          headers: {
+            Authorization: `Bearer ${res.access_token}`,
+          },
+        });
+      });
+
+      // 上記で返ってきたPromise<AxiosResponse<Message>>を調理
+      Promise.all(data).then((result) => {
+        const arr = result.map((r) => r.data);
+        setMails(arr);
+      });
+    }
+  }, [mailList]);
+
+  // subjectはheaders（配列）の中のnameが"Subject"となっているものなので、それ用の関数
+  // headers: [
+  //   {
+  //     name: "Subject"
+  //     value: "メールのタイトルが入ります。"
+  //   },
+  //   {
+  //     name: "Subject"
+  //     value: "メールのタイトルが入ります。2"
+  //   }
+  // ]
+  const getSubject = (mail: Message): string => {
+    const sub = mail.payload.headers.filter((h) => h.name === "Subject");
+    if (sub != null && sub.length > 0) {
+      return sub[0].value;
+    }
+    return "not found subject";
+  };
+
+  return (
+    <div>
+      <p>login page.</p>
+      {/*このボタンを押したら認証*/}
+      <button
+        onClick={() => {
+          window.location.href = gmailAuthEndpoint;
+        }}
+      >
+        認証
+      </button>
+      <p>{code}</p>
+      {hasError && <p>エラーがあります。再認証してください。</p>}
+      {invalidCode && <p>認証に失敗しました。再度認証してください。</p>}
+      {/* 認証後にAccessTokenが取得出来たらここに表示するようにする */}
+      {res && (
+        <>
+          <ul>
+            <li>accessToken: {res.access_token}</li>
+            <li>refreshToken: {res.scope}</li>
+            <li>expiresIn: {res.expires_in}</li>
+            <li>tokenType: {res.token_type}</li>
+          </ul>
+          <button onClick={getProfile}>プロフィール取得</button>
+        </>
+      )}
+      {
+        // Profileが取得出来たらここに表示するようにする。
+        profile && (
+          <>
+            <ul>
+              <li>emailAddress: {profile.emailAddress}</li>
+              <li>historyId: {profile.historyId}</li>
+              <li>messagesTotal: {profile.messagesTotal}</li>
+              <li>threadsTotal: {profile.threadsTotal}</li>
+            </ul>
+            <button onClick={getEmail}>get mail</button>
+          </>
+        )
+      }
+      {
+        // mailの詳細get後に一気に表示されるようにする。
+        mails.length > 0 && (
+          <>
+            {mails.map((m, index) => {
+              return (
+                <>
+                  <ul>
+                    <li>{getSubject(m)}</li>
+                    <li>snippet: {m.snippet}</li>
+                  </ul>
+                </>
+              );
+            })}
+          </>
+        )
+      }
+    </div>
+  );
+};
+
+export default Index;
